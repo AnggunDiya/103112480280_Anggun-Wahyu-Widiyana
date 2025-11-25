499c8ec3aeb2999561785ab950dbef4b325ae522
AnggunDiya <anggunwdy7@gmail.com>
2025-11-25 08:56:19 +0700

    Add files via upload

---
+++ UJIAN STRUKDAT/Queue/SoalQueue.cpp
@@ -0,0 +1,20 @@
+/*file.queue.h*/
+#ifndef queue_h
+#define queue_h
+
+#define Nil NULL 
+using namespace std;
+
+typedef int infotype;
+struct elmList *address;
+
+struct paket{
+    string kodeResi;
+    string namaPengirim;
+    int beratBadan(kg);
+    string tujuan;
+
+};
+
+
+
