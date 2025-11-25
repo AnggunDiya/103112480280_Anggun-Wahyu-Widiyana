ee70b3864733c3af0509fb5977700d9be9ddf99e
AnggunDiya <anggunwdy7@gmail.com>
2025-11-25 09:08:32 +0700

    Update SoalQueue.cpp

--- UJIAN STRUKDAT/Queue/SoalQueue.cpp
+++ UJIAN STRUKDAT/Queue/SoalQueue.cpp
@@ -1,20 +1,5 @@
-/*file.queue.h*/
-#ifndef queue_h
-#define queue_h
 
-#define Nil NULL 
-using namespace std;
 
-typedef int infotype;
-struct elmList *address;
-
-struct paket{
-    string kodeResi;
-    string namaPengirim;
-    int beratBadan(kg);
-    string tujuan;
-
-};
 
 
 
